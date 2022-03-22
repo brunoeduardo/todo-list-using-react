b6e63a7707e19cb96ad523c9d75187fba3f001e0
Bruno Eduardo <bruno@beduardo.com>
2022-03-21 21:28:30 -0300

    add card style and priority typys

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ let initialStateActivity = [
   {
     id: 1,
     description: "First Activity",
+    title: "First Title",
+    priority: 1,
   },
   {
     id: 2,
     description: "Second Activity",
+    title: "Second Title",
+    priority: 2,
   },
 ];
 
@@ -21,9 +25,31 @@ function App() {
     const activityItem = {
       id: parseInt(document.getElementById("id").value),
       description: document.getElementById("description").value,
+      title: document.getElementById("title").value,
+      priority: document.getElementById("priority").value,
     };
 
     setActivity([...activities, { ...activityItem }]);
+  }
+
+  function priorityLabel(param) {
+    const Priority = {
+      1: "low",
+      2: "normal",
+      3: "high",
+    };
+
+    return Priority[param] || " - ";
+  }
+
+  function priorityLabelIcon(param) {
+    const Priority = {
+      1: "smile",
+      2: "meh",
+      3: "frown",
+    };
+
+    return Priority[param] || " - ";
   }
 
   return (
@@ -38,6 +64,28 @@ function App() {
             type="number"
             className="form-control"
             placeholder="Id"
+            value={activities.length + 1}
+            disabled
+          />
+        </div>
+        <div className="col-md-6">
+          <label className="form-label">Priority</label>
+          <select id="priority" className="form-select">
+            <option defaultValue="0">Choose...</option>
+            <option value="1">Low</option>
+            <option value="2">Normal</option>
+            <option value="3">High</option>
+          </select>
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            id="title"
+            type="text"
+            className="form-control"
+            placeholder="Title"
           />
         </div>
         <div className="col-md-6">
@@ -62,13 +110,39 @@ function App() {
       <hr />
       <div className="mt-3 mt-4">
         {activities.map((el) => (
-          <div key={el.id} className="card mb-2">
+          <div
+            key={el.id}
+            className={"card mb-2 " + priorityLabel(el.priority)}
+          >
             <div className="card-body">
               <div className="d-flex justify-content-between">
-                <h5 className="card-title">{el.id} - title</h5>
-                <h6>Priority: Normal</h6>
+                <h5 className="card-title">
+                  {el.id} - {el.title}
+                </h5>
+                <h6>
+                  Priority:
+                  <i
+                    className={
+                      "ms-1 me-1 fa-regular label-priority fa-face-" +
+                      priorityLabelIcon(el.priority)
+                    }
+                  ></i>
+                  <span className="label-priority">
+                    {priorityLabel(el.priority)}
+                  </span>
+                </h6>
               </div>
               <p className="card-text">{el.description}</p>
+              <div className="d-flex justify-content-end">
+                <button className="btn btn-outline-primary me-2">
+                  <i className="fas fa-pen me-2"> </i>
+                  Edit
+                </button>
+                <button className="btn btn-outline-danger">
+                  <i className="fas fa-trash me-2"> </i>
+                  Delete
+                </button>
+              </div>
             </div>
           </div>
         ))}
